aa006066de9c720d9e1e2022dfce3ae4fae21b16
Phantom_Cloak18 <131883829+rijusmit22400@users.noreply.github.com>
2024-05-29 21:03:14 +0530

    jwt tokens made

--- krsr/store_backend/server.py
+++ krsr/store_backend/server.py
@@ -1,8 +1,13 @@
-from flask import Flask, request, redirect
+from urllib import response
+from flask import Flask, request, redirect, session
 import db_connect as db
 from api_methods import validate, add_user, add_customer
 import json as js
 from flask_cors import CORS
+import jwt_tokens as tokens
+from flask import jsonify
+
+secret = tokens.secret_key
 
 
 ses = db.session()
@@ -13,6 +18,7 @@ PRODUCT = db.Product
 CATEGORY = db.Category
 
 app = Flask(__name__)
+app.secret_key = secret
 
 CORS(app,origins="http://localhost:5173", allow_headers="Content-Type", supports_credentials=True)
 
@@ -25,10 +31,19 @@ def login():
     password_hasher = {i.username: i.password for i in users}
     usernames = [i.username for i in users]
     status = validate(username, password, usernames, password_hasher)
-    print(status)
     if(status[0]==False):
         return redirect("http://localhost:5173/auth_register")
+    payload = {"payload": {"username": username, "key": str(password_hasher[username])}}
+    auth_token = tokens.generate_token({"payload": payload})
+    session['auth_token'] = auth_token
+    session['username'] = username
+    e_paylaod = {"payload": "payload", "token": auth_token}
     return redirect(f"http://localhost:5173/home?username={username}&key={str(password_hasher[username])}")
+
+
+@app.route('/auth_payload', methods=['GET'])
+def look(payload):
+    return jsonify(payload)
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -44,11 +59,8 @@ def register():
     usernames = [i.username for i in users]
     status_user = add_user(username, password, full_name, email, ses, usernames)
     if(status_user[0]):
-        status_customer = add_customer(len(users)+1, contact, address, ses)
-        print(status_customer)
-    print(status_user)
+        add_customer(len(users)+1, contact, address, ses)
     ses.commit()
-    print("comitted")
     return redirect("http://localhost:5173/auth_login")
 
 @app.route('/call/product/<id>', methods=['GET'])
@@ -56,7 +68,6 @@ def product(id):
     products = ses.query(PRODUCT).filter(PRODUCT.category_id == id).all()
     print("Request Accpeted ,Product")
     category = ses.query(CATEGORY).filter(CATEGORY.id == id).first()
-    print(category)
     output = { "name": category.description, "items":[{"item":products[i].name, "description":products[i].description, "price":products[i].price, "stock":products[i].quantity} for i in range(len(products))]}
     return output
 
