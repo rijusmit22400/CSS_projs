af755c669302f9eabaa296828336685199ec2783
Phantom_Cloak18 <131883829+rijusmit22400@users.noreply.github.com>
2024-06-07 00:48:49 +0530

    project-done

--- krsr/krsr_store/src/Landing.jsx
+++ krsr/krsr_store/src/Landing.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useState,useEffect } from 'react';
 
 function LandingPage() {
-  let name = "guest";
-  let key = "no-key";
-  name = localStorage.getItem("user") ? localStorage.getItem("user") : "guest";
-  key = localStorage.getItem("key") ? localStorage.getItem("key") : "no-key";
-  console.log(name);
-  console.log(key);
+  const [name, setName] = useState("guest");
+  const [key, setKey] = useState("no-key");
 
+  useEffect(() => {
+    setName(localStorage.getItem("user") ? localStorage.getItem("user") : "guest");
+    setKey(localStorage.getItem("key") ? localStorage.getItem("key"): "no-key");
+  });
   return <>
   <Navigate to={`/home?username=${name}&key=${key}`}></Navigate>
   </>;
